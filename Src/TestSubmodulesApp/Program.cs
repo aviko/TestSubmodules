431b917ccbcb3300e5b935e3c5c59babcdf0d99a
aviko <aviko@proline.ltd>
2019-12-09 15:03:23 +0200

    Add TestSubmodulesApp

---
+++ Src/TestSubmodulesApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestSubmodulesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
