35355271c12181cb4109fb0cb70cc2bc36710f9d
aviko <aviko@proline.ltd>
2019-12-09 15:26:34 +0200

    use submodule

--- Src/TestSubmodulesApp/Program.cs
+++ Src/TestSubmodulesApp/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace TestSubmodulesApp
 {
+
+    class TestPoco
+    {
+        public int IntVal { get; set; }
+        public string StringVal { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var origPoco = new TestPoco { IntVal = 7, StringVal = "49" };
+
+            var buf = BinaryConverter.BinaryConvert.SerializeObject(origPoco);
+
+            var clonedPoco = BinaryConverter.BinaryConvert.DeserializeObject<TestPoco>(buf);
+
+            Console.WriteLine(JsonConvert.SerializeObject(clonedPoco));
+
         }
     }
 }
